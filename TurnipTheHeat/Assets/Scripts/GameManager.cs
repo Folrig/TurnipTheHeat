0ce452bbc317249cfcf1bc4b14d2b5b78a086adf
Ianmring <Ianmring@gmail.com>
2018-09-30 16:28:28 -0700

    fixed game manager

--- TurnipTheHeat/Assets/Scripts/GameManager.cs
+++ TurnipTheHeat/Assets/Scripts/GameManager.cs
@@ -8,15 +8,11 @@ public class GameManager : MonoSingleton<GameManager>
     public float speed;
     public float rate;
 
-<<<<<<< HEAD
-    bool timegogo;
 
     public float projspeed;
 
     public float porjtime;
-=======
    public bool timegogo;
->>>>>>> 7f796154679884755fd5cb12d6a0b785eef8fbee
     protected override void Initiate()
     {
         
