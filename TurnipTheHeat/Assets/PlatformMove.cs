8bd17b8a780454e1f300aa4c6e750323cd304c13
uvasq <uvasquezve@student.rccd.edu>
2018-09-30 12:39:02 -0700

    Falling...

--- TurnipTheHeat/Assets/PlatformMove.cs
+++ TurnipTheHeat/Assets/PlatformMove.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour {
     public float speed = 1.0f;
-    GameManager control;
-	// Use this for initialization
-	void Start () {
-        control = GameObject.FindObjectOfType<GameManager>();
+    public float platformMinXSize = 3.0f;
+    public float platformMaxXSize = 7.0f;
+    public float platformYSize = 0.4f;
+    public float platformZSize = 1.0f;
+    public Vector3 platformSize;
 
-        speed = (control.Timego * control.rate) + 1.0f;
+    private void Awake()
+    {
+        platformSize = new Vector3(Random.Range(platformMinXSize, platformMaxXSize), platformYSize, platformZSize);
+        gameObject.transform.localScale = platformSize;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         gameObject.transform.Translate(Vector3.down * speed * Time.deltaTime);
-	}
+    }
+
+
 }
--- TurnipTheHeat/Assets/PlatformSpawner.cs
+++ TurnipTheHeat/Assets/PlatformSpawner.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour {
-    public GameObject prefab = null;
+    public GameObject prefab;
     
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(DelayedSpawn(1f, 2f));
+        StartCoroutine(DelayedSpawn(2f, 4f));
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-  
+         transform.position = new Vector3(Random.Range(-7.0f, 7.0f), 7.5f, 0);
+    }
+
+    void NewSpawnLocation()
+    {
+
     }
 
     IEnumerator DelayedSpawn(float minTime, float maxTime) {
         while (isActiveAndEnabled)
         {
+
             yield return new WaitForSeconds(minTime);
             Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
         }
