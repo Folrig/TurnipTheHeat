c252238ca7d62db485640208858f6357c3f363b7
Dorma <dorma@dinoleaf.com>
2018-09-30 11:21:56 -0700

    PROPER fun stuff
    Woops

---
+++ TurnipTheHeat/Assets/Scripts/ComponentCache.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Semi-automated component caching container for quick and safe component caching.
+/// 
+/// [Example Use] easily giving a MonoBehaviour a cache to its Transform...
+/// 
+/// ComponentCache<Transform> _transform = new ComponentCache<Transform>();
+/// Transform MyTransform {get{return _transform.Get(this);}}
+/// -Dorma Oculus
+/// </summary>
+
+public class ComponentCache<T> where T : Component {
+	#region Members
+	private T _cache = null;
+	#endregion
+
+	#region Properties
+	/// <summary>
+	/// Gets a value indicating whether the cache points to anything.
+	/// </summary>
+	/// <value><c>true</c> if the cache is safe; otherwise, <c>false</c>.</value>
+	public bool IsSafe {get{return(_cache != null);}}
+	#endregion
+
+	#region Get cache reference
+	/// <summary>
+	/// Get the cached component from a given MonoBehaviour reference.
+	/// Required to be called at least once so a cache can be made from given MonoBehaviour.
+	/// </summary>
+	/// <param name="mGameObject">M game object.</param>
+	public T Get(MonoBehaviour mTarget) {
+		if (!_cache)
+			LinkCache (mTarget);
+		return _cache;
+	}
+
+	/// <summary>
+	/// Get the cached component.
+	/// </summary>
+	public T Get() {
+		if (!_cache)
+			Debug.LogError("Attempted to cache nonexistent component. Get(MonoBehaviour) must be called at least once before Get().");
+		return _cache;
+	}
+	#endregion
+
+	#region Constructors
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ComponentCache`1"/> class.
+	/// </summary>
+	public ComponentCache() {}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ComponentCache`1"/> class.
+	/// Instantly links cache from the given GameObject.
+	/// </summary>
+	/// <param name="mTarget">M target.</param>
+	public ComponentCache(MonoBehaviour mTarget) {
+		LinkCache(mTarget);
+	}
+	#endregion
+
+	#region Link cache
+	/// <summary>
+	/// Caches the T reference from a given MonoBehaviour
+	/// </summary>
+	/// <returns><c>true</c>, if cache was linked, <c>false</c> if an error is encountered.</returns>
+	/// <param name="mTarget">M target.</param>
+	private bool LinkCache(MonoBehaviour mTarget) {
+		_cache = mTarget.GetComponent<T>();
+
+		if (!mTarget) {
+			Debug.LogError ("Attempted to cache a component " + typeof(T).ToString() + " cache to to null GameObject.");
+			return false;
+		}
+	
+		if (!_cache) {
+			Debug.LogError("Attempted to cache nonexistent component " + typeof(T).ToString() + " on " + mTarget);
+			return false;
+		}
+		return true;
+	}
+	#endregion
+}
---
+++ TurnipTheHeat/Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,149 @@
+﻿/*
+ * Author: Dorma Okulo
+*/
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A Generic singleton class powered by Unity's MonoBehaviour.
+/// 
+/// Override Initiate() instead of using Awake() for things that should
+/// run before Start().
+/// -Dorma
+/// </summary>
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : Component {
+	#region Constants
+	private const string
+		NAME_PREFIX = "Singleton(",
+		NAME_SUFFIX = ")";
+	#endregion
+
+	#region Members
+	private static T _instance = null;
+	private static bool _isExiting = false;
+	#endregion
+
+	#region Properties
+	/// <summary>
+	/// Get the current singleton instance.
+	/// </summary>
+	/// <value>The instance.</value>
+	public static T Instance {
+		get{
+			if (_instance != null)
+				return _instance;
+			else if (_instance == null && !_isExiting) {
+				_instance = CreateInstance();
+				Debug.LogWarning("Created " + _instance.name + " as none was detected.");
+				return _instance;
+			}
+			return null;
+		}
+	}
+	#endregion
+
+	#region MonoBehaviour
+	private void Awake() {
+		VerifySingleton();
+		DontDestroyOnLoad(gameObject);
+		Initiate();
+	}
+
+	private void OnApplicationQuit() {
+		_isExiting = true;
+	}
+	#endregion
+
+	/// <summary>
+	/// Initiate this instance. Use this in favor of Awake().
+	/// </summary>
+	protected abstract void Initiate();
+
+	#region Creation
+	/// <summary>
+	/// Creates an instance of the singleton.
+	/// </summary>
+	/// <returns>The instance.</returns>
+	private static T CreateInstance() {
+		string tObjName = NAME_PREFIX + typeof(T).Name + NAME_SUFFIX;
+
+		// Check if, for any reason, the GameObject of this name exists and create
+		// a new one if not. Recycle the found instance if so.
+		// This way, the singleton can't be uninitialized and initialized to 
+		// create endless copies of GameObjects.
+		GameObject tNewSingleton = GameObject.Find(tObjName);
+		if (tNewSingleton == null)
+			tNewSingleton = new GameObject(tObjName);
+
+		T tInstance = tNewSingleton.AddComponent<T>();
+		return tInstance;
+	}
+	#endregion
+
+	#region Verification
+	/// <summary>
+	/// Verifies the singleton.
+	/// </summary>
+	/// <returns><c>true</c>, if this is the singleton, <c>false</c> if error.</returns>
+	protected bool VerifySingleton() {
+		// Kill self if a singleton is already established
+		if (_instance != null) {
+			Destroy(gameObject);
+			return false;
+		}
+
+		T[] tInstances = FindObjectsOfType<T>();
+
+		// Not sure which instance should be the singleton.
+		if (tInstances.Length > 1 && _instance == null) {
+			Debug.LogError("Multiple instances of a singleton detected, not sure which to set reference.",this);
+			Debug.Break();
+		}
+
+		// I am the singleton
+		if (tInstances.Length == 1 && _instance == null) {
+			_instance = this as T;
+			return true;
+		}
+
+		return false;
+	}
+	#endregion
+
+	#region Unloading
+	/// <summary>
+	/// Unloads the singleton.
+	/// </summary>
+	public void UnloadSingleton() {
+		UnloadSingleton(true, false);
+	}
+
+	/// <summary>
+	/// Unloads the singleton.
+	/// </summary>
+	/// <param name="mDestroyComponent">If set to <c>true</c> destroys the component.</param>
+	public void UnloadSingleton(bool mDestroyComponent) {
+		UnloadSingleton(mDestroyComponent,false);
+	}
+
+	/// <summary>
+	/// Unloads the singleton.
+	/// </summary>
+	/// <param name="mDestroyComponent">If set to <c>true</c> destroys the component.</param>
+	/// <param name="mDestroyObject">If set to <c>true</c> destroys the GameObject.</param>
+	public void UnloadSingleton(bool mDestroyComponent, bool mDestroyObject) {
+		string tMsg = (mDestroyObject & mDestroyComponent)
+			? name + " reference was unloaded and destroyed."
+			: name + " reference was unloaded.";
+		Debug.Log (tMsg);
+		_instance = null;
+
+		if (mDestroyComponent)
+			Destroy(this);
+		if (mDestroyObject)
+			Destroy(gameObject);
+	}
+	#endregion
+}
