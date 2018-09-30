fcecd62ed53577319a5cb7274bb7c82ec7e27305
James <jspencer30@student.rccd.edu>
2018-09-30 11:11:25 -0700

    Folder Creation

---
+++ TurnipTheHeat/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Methods
+    void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        bool jump = Input.GetButtonDown("Jump");
+
+
+    }
+    #endregion
+}
