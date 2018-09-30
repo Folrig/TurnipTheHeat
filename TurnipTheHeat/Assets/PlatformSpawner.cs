4d3c757a2c97a77070f9e02c7c6b40e2125cd23e
uvasq <uvasquezve@student.rccd.edu>
2018-09-30 11:48:24 -0700

    Falling Stuff
    I did falling stuff

---
+++ TurnipTheHeat/Assets/PlatformSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+    public GameObject prefab = null;
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DelayedSpawn(1f, 2f));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+  
+    }
+
+    IEnumerator DelayedSpawn(float minTime, float maxTime) {
+        while (isActiveAndEnabled)
+        {
+            yield return new WaitForSeconds(minTime);
+            Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
