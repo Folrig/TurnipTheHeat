772b1ec2e2f644514b995e99d82d39ddea40bc1a
uvasq <uvasquezve@student.rccd.edu>
2018-09-30 20:41:49 -0700

    Initialization scene

---
+++ TurnipTheHeat/Assets/Scripts/Menus/InitScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitScreen : MonoBehaviour {
+    private void Awake() {
+        StartCoroutine(WaitNGo());
+    }
+
+    IEnumerator WaitNGo() {
+        yield return new WaitForSeconds(4.0f);
+        SceneManager.LoadScene("Boot");
+    }
+}
